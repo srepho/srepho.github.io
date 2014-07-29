3fdb77814e09b31177e515712b56c95befcc7480
srepho <stephen.j.a.oates@gmail.com>
2014-07-29 19:16:34 +1000

    Further work.

--- Churn/Script.R
+++ Churn/Script.R
@@ -5,3 +5,9 @@ library(ggplot2)
 library(reshape2)
 library(caret)
 
+
+set.seed(12)
+trainIndex <- caret::createDataPartition(churn$Churn., p = .75, list = FALSE, times = 1)
+churnTrain <- churn[ trainIndex,]
+churnTest <- churn[-trainIndex, ]
+
