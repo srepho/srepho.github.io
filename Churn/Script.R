5da05c289ab3f0ad869b76418b7f42a7a748cdb0
srepho <stephen.j.a.oates@gmail.com>
2014-07-30 00:34:37 +1000

    Ran RF and confusion matrix

--- Churn/Script.R
+++ Churn/Script.R
@@ -11,3 +11,11 @@ trainIndex <- caret::createDataPartition(churn$Churn., p = .75, list = FALSE, ti
 churnTrain <- churn[ trainIndex,]
 churnTest <- churn[-trainIndex, ]
 
+churnTrain$Phone<-NULL
+churnTest$Phone<-NULL
+churnTrain$Area.Code<-as.factor(churnTrain$Area.Code)
+churnTest$Area.Code<-as.factor(churnTest$Area.Code)
+
+set.seed(12)
+lrmodel<-train(churnTrain$Churn.~., data=churnTrain, method="rf", trainControl = c(method = "adaptive_cv", number = 10, repeats = 5, classProbs = TRUE, summaryFunction = twoClassSummary, adaptive = list(min = 10, alpha = 0.05, method = "gls", complete = TRUE)), metric="Kappa")
+
