49e5423a7feab66c586e73744fe1561ef1684d1b
srepho <stephen.j.a.oates@gmail.com>
2014-08-28 23:54:18 +1000

    script start

--- AfricaSoil/script.R
+++ AfricaSoil/script.R
@@ -6,3 +6,30 @@ library(caret)
 
 
 predictors<-select(training, SOC, pH, Ca, P, Sand)
+train<-select(training, -(PIDN))
+train<-select(training, -(Ca:Sand))
+test<-select(sorted_test, -(PIDN))
+
+dtrain<-select(train, Depth)
+train<-select(train, -(Depth))
+dtest<-select(test, Depth)
+test<-select(test, -(Depth))
+#Plotting
+
+ggplot(predictors, aes(x=SOC)) + geom_histogram()
+ggplot(predictors, aes(x=pH)) + geom_histogram()
+ggplot(predictors, aes(x=Ca)) + geom_histogram()
+ggplot(predictors, aes(x=P)) + geom_histogram()
+ggplot(predictors, aes(x=Sand)) + geom_histogram()
+
+
+#Remove NZV and correlations
+
+nzv <- nearZeroVar(train)
+#Nothing!
+
+
+foo<-cor(train)
+
+highlyCorDescr <- findCorrelation(train)
+filteredDescr <- filteredDescr[, -highlyCorDescr]
