9bba26cf0f3bbc848e26280f46ba1f3e0086c4df
srepho <stephen.j.a.oates@gmail.com>
2014-10-26 00:25:40 +1100

    SFPD stuff

--- SFCrime/SFCrimeScript.R
+++ SFCrime/SFCrimeScript.R
@@ -1,17 +1,11 @@
 library(dplyr)
 library(ggplot2)
 library(caret)
-
-library(RgoogleMaps)
-center = c(-122.4,37.77)
-center = c(37.77, -122.4)
-zoom <- 5 
-terrmap <- GetMap(center=center, zoom=zoom, maptype= "satellite", destfile = "terrain1.png")
-plot(terrmap)
+library(ggmap)
 
 
-BrooklynMap <- GetMap(center="San Francisco", zoom=13)
+sf<-qmap("San Francisco", zoom = 13, maptype= "satellite")
+sf+geom_point(aes(x=X, y=Y), data=SFPD)
 
-
-baylor <- "baylor university"
-qmap(baylor, zoom = 14)
+SFPD$Date<-as.Date(SFPD$Date, format="%M/%d/%Y")
+head(foo)
