f95baf99c8b3f0c524ee63cbd45cc94669f9d5a8
srepho <stephen.j.a.oates@gmail.com>
2014-10-26 18:57:35 +1100

    starting with time series

--- SFCrime/SFCrimeScript.R
+++ SFCrime/SFCrimeScript.R
@@ -3,9 +3,29 @@ library(ggplot2)
 library(caret)
 library(ggmap)
 
-
-sf<-qmap("San Francisco", zoom = 13, maptype= "satellite")
-sf+geom_point(aes(x=X, y=Y), data=SFPD)
-
 SFPD$Date<-as.Date(SFPD$Date, format="%M/%d/%Y")
-head(foo)
+
+SFPD$DT <- paste(SFPD$Date, SFPD$Time, sep=" ")
+
+SFPD$DT<-as.POSIXct(SFPD$DT, format="%Y-%M-%d %H:%M", tz="America/Los_Angeles")
+
+#Total:
+
+sf<-qmap("San Francisco", zoom = 13, maptype= "hybrid")
+sf+geom_point(aes(x=X, y=Y, color=Category), data=SFPD)
+
+#Time Series
+
+SFTS <- SFPD %>%
+  group_by(DT) %>%
+  summarise(
+    Total=n()
+  )
+
+ggplot(SFTS, aes(x=DT, y=Total)) + geom_line() + scale_x_datetime()
+
+#Weekday
+
+#Friday and Saturday Evenings
+
+#Weekend
