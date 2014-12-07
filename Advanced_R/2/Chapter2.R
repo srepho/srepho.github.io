0470d4b7846f3a5b35c6e7844263cb3e80faab88
srepho <stephen.j.a.oates@gmail.com>
2014-12-08 00:40:22 +1100

    more study

--- Advanced_R/2/Chapter2.R
+++ Advanced_R/2/Chapter2.R
@@ -81,3 +81,91 @@ levels(f1) <-rev(levels(f1))
 #3 What does this code do? How do f2 and f3 differ from f1?
 f2 <- rev(factor(letters))
 f3 <- factor(letters, levels = rev(letters))
+
+
+
+
+### Chapter 4
+
+# The first functions to learn
+?
+str
+
+# Important operators and assignment
+%in%, match
+=, <-, <<-
+  $, [, [[, head, tail, subset
+with
+assign, get
+          
+          # Comparison 
+          all.equal, identical
+          !=, ==, >, >=, <, <=
+            is.na, complete.cases
+          is.finite
+          
+          # Basic math
+          *, +, -, /, ^, %%, %/%
+            abs, sign
+          acos, asin, atan, atan2
+          sin, cos, tan
+          ceiling, floor, round, trunc, signif
+          exp, log, log10, log2, sqrt
+          
+          max, min, prod, sum
+          cummax, cummin, cumprod, cumsum, diff
+          pmax, pmin
+          range
+          mean, median, cor, sd, var
+          rle
+          
+          # Functions to do with functions
+          function
+          missing
+          on.exit
+          return, invisible
+          
+          # Logical & sets 
+          &, |, !, xor
+          all, any
+          intersect, union, setdiff, setequal
+          which
+          
+          # Vectors and matrices
+          c, matrix
+          # automatic coercion rules character > numeric > logical
+          length, dim, ncol, nrow
+          cbind, rbind
+          names, colnames, rownames
+          t
+          diag
+          sweep
+          as.matrix, data.matrix
+          
+          # Making vectors 
+          c
+          rep, rep_len
+          seq, seq_len, seq_along
+          rev
+          sample
+          choose, factorial, combn
+          (is/as).(character/numeric/logical/...)
+          
+          # Lists & data.frames 
+          list, unlist
+          data.frame, as.data.frame
+          split
+          expand.grid
+          
+          # Control flow 
+          if, &&, || (short circuiting)
+          for, while
+          next, break
+          switch
+          ifelse
+          
+          # Apply & friends
+          lapply, sapply, vapply
+          apply
+          tapply
+          replicate
